590b2fe29adb07a71aee09a07f8121ddf4eab31f
Kris1027 <obarzanek.work@gmail.com>
2023-09-14 22:18:28 +0200

    Create Header component

--- src/features/pages/Header.jsx
+++ src/features/pages/Header.jsx
@@ -1,7 +1,30 @@
 function Header() {
   return (
-    <div>
-      <h1>sneakers</h1>
+    <div className='flex justify-between w-full p-6'>
+      <div className='flex gap-4'>
+        <img
+          src='../src/assets/icon-menu.svg'
+          alt='menu icon'
+          className='h-6'
+        />
+        <img
+          src='../src/assets/logo.svg'
+          alt='logo of the shop'
+          className='h-6'
+        />
+      </div>
+      <div className='flex gap-4'>
+        <img
+          src='../src/assets/icon-cart.svg'
+          alt='icon of the cart'
+          className='h-6'
+        />
+        <img
+          src='../src/assets/image-avatar.png'
+          alt='profile photo'
+          className='h-6'
+        />
+      </div>
     </div>
   );
 }
--- src/features/pages/Home.jsx
+++ src/features/pages/Home.jsx
@@ -1,9 +1,5 @@
 function Home() {
-  return (
-    <div>
-      <h1>elo</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Home;
